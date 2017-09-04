484d7f46c9b003e4d03ec291620631b54c410e94
Rob Newport <robnewport@gmail.com>
2017-09-03 17:28:41 -0700

    Initial Commit

---
+++ TestLib/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestLib
+//
+//  Created by Rob Newport on 9/3/17.
+//  Copyright © 2017 Whisteo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestLib/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestLib
+//
+//  Created by Rob Newport on 9/3/17.
+//  Copyright © 2017 Whisteo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
